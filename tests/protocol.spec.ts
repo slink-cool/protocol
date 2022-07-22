726e9f8e08c61ee8bf925c62895dd04464a04b68
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-07-22 12:59:40 +0400

    Anchor 0.2.5

--- tests/protocol.spec.ts
+++ tests/protocol.spec.ts
@@ -1,21 +1,15 @@
-import {
-  Program,
-  Provider,
-  setProvider,
-  workspace,
-} from '@project-serum/anchor';
+import { Program, setProvider, workspace } from '@project-serum/anchor';
 import type { Protocol } from '../target/types/protocol';
+import { AnchorProvider } from '@project-serum/anchor/dist/cjs/provider';
 
 describe('protocol', () => {
-  // Configure the client to use the local cluster.
-  setProvider(Provider.env());
+  setProvider(AnchorProvider.env());
 
   const program = workspace.Protocol as Program<Protocol>;
 
   it('Is initialized!', async () => {
-    console.log(process.env.ANCHOR_PROVIDER_URL);
     // Add your test here.
-    const tx = await program.rpc.initialize({});
+    const tx = await program.methods.initialize();
 
     console.log('Your transaction signature', tx);
   });
