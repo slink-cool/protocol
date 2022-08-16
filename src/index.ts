511a66ab8164bc3110014e0a7f91d91d99f423df
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-08-16 22:27:25 +0400

    Commit build scripts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from 'program/idl'
+export * from 'program/program-api'
+export * from 'program/storage-api'
+export * from 'slink/model'
+export * from 'slink/slink-api'
+export * from 'utils/wallet-adapter'
+export * from 'utils/node-wallet-adapter'
